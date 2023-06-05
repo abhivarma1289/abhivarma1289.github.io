76e18e233804e70d1518d7c984d44b0b6cc25ee4
abhivarma1289 <112379996+abhivarma1289@users.noreply.github.com>
2023-06-05 21:54:14 +0530

    pics uploded

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-alert("hello abhi varma !!")
-console.log("abhi varma")
+var data1={image : "abc.jpg",name : " K SAI YUGANDHAR", designation:"Intern"};
+var data2={image : "edf.jpg",name : "YUGANDHAR", designation:"Software Devoloper"};
+var isdata1 = true;
+var displaydata;
+var changemypic = function(){
+    if(isdata1){
+        displaydata= data2;
+        isdata1 = false;
+    }
+    else{
+        displaydata = data1;
+        isdata1=true;
+    }
+    document.getElementById("myimage").src=displaydata.image;
+}
