06da822ff8094acca85957153452975bca1ca04e
abhivarma1289 <112379996+abhivarma1289@users.noreply.github.com>
2023-06-07 15:00:55 +0530

    fixed naming in card2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var data1={image : "abc.jpg",name : " K SAI YUGANDHAR", designation:"Intern"};
-var data2={image : "edf.jpg",name : "YUGANDHAR", designation:"Software Devoloper"};
+var data1={image :"passphoto.jpg",name : " Dongari Abhilash Varma", designation:"Intern"};
+var data2={image : "heroImage.jpg",name : "Abhilash varma", designation:"Software Devoloper"};
 var isdata1 = true;
 var displaydata;
 var changemypic = function(){
